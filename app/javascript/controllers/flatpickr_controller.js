fb8eb010990b9ff18e3c4de37c5577fa6a612196
Emeline Barthe <bartheemeline@gmail.com>
2022-11-23 17:46:47 +0100

    flatpickr set up + bookings

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -1,12 +1,17 @@
-import { Controller } from "@hotwired/stimulus";
-import flatpickr from "flatpickr";
-
-// Connects to data-controller="flatpickr"
+import { Controller } from "@hotwired/stimulus"
+// Import flatpickr function
+import flatpickr from "flatpickr"
+// The range plugin is also needed in order to use two inputs
+import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
 export default class extends Controller {
-  connect() {
-      new flatpickr(this.element, {
-        enableTime: true
-        // more options available on the documentation!
-      });
-    }
-  }
+// retrieve the targets from the DOM
+static targets = [ 'startDateInput', 'endDateInput' ]
+// On controller's connection, call the flatpickr
+// function in order to build the calendars
+connect() {
+flatpickr(this.startDateInputTarget, {
+mode: 'range',
+"plugins": [new rangePlugin({ input: this.endDateInputTarget})]
+})
+}
+}
